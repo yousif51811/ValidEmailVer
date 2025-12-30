40c423b819b671d85743ebd1bbb9ef8bab791388
Ownerbaby <slogxtt@gmail.com>
2025-12-31 03:13:58 +0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Emailcheck
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter you email address:");
+            string? email = Console.ReadLine();
+            try
+            {
+                int atlocation = email.IndexOf("@");
+                string? Domain = email.Substring((atlocation + 1));
+                string? Username = email.Substring(0,atlocation);
+                int dotlocation = email.IndexOf(".");
+                if (dotlocation <= 0 )
+                {
+                    throw new Exception("Invalid Email!");
+                }
+                Console.WriteLine($"Username: {Username}");
+                Console.WriteLine($"Domain: {Domain}");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid email!");
+            }
+        }
+    } 
+}
