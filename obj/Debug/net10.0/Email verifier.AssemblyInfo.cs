1ffdf28537df3c77d22164b910bd6a723cf7abb1
yousif51811 <yousif51811@gmail.com>
2025-12-31 04:01:56 +0400

    Deleted .gitignore

--- obj/Debug/net10.0/Email verifier.AssemblyInfo.cs
+++ obj/Debug/net10.0/Email verifier.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Email verifier")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4e49fa572611ad591fb5151595f9c19a65d3816")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+200c72055e2b342ae06f526bd22896e095af20b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Email verifier")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Email verifier")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
